e346396ebe3341bdee287aa3464151f4a9fd0953
Махмутов Владимир Александрович <mva@csm.local>
2016-08-14 16:59:59 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/images/ico_tower.ico" type="image/x-icon">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="reywords" content="автовышки, автовышка, услуги автовышек, Томск, Северск, автовышкин, Автовышкин, услуги автовышек в Томске">
+    <meta name="description" content="Услуги автовышек в Томске и Северске">
+    <meta name="author" content="6AHK0MAT">
+
+    <link href="css/reset.css" rel="stylesheet" type="text/css" />
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Автовышкин</title>
+
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+
+    <![endif]-->
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+
+    <!--Фон сайта
+    <style>
+        body {
+            background-image: url(images/0370.png); /* Путь к фоновому изображению */
+             }
+    </style>
+    -->
+
+    <!-- Start WOWSlider.com HEAD section -->
+    <link rel="stylesheet" type="text/css" href="engine1/style.css" />
+    <script type="text/javascript" src="engine1/jquery.js"></script>
+    <!-- End WOWSlider.com HEAD section -->
+
+</head>
+<body>
+
+<!--
+<div class="navbar navbar-inverse navbar-static-top header-0370">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-header img-0370" href="index.php"><img src="images/0370_150dpi.png"></a>
+        </div>
+        <div class="collapse navbar-collapse phone-0370">
+            <img src="images/red_phone_150dpi.png">
+        </div>
+    </div>
+</div>
+-->
+
+
+
+<div class="navbar navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+
+            <button class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
+
+                <span class="sr-only">Открыть навигацию</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="responsive-menu">
+            <ul class="nav navbar-nav">
+                    <li><a href="#">Главная</a></li>
+                    <li><a href="#">Прайс</a></li>
+                    <li><a href="#">Информация</a></li>
+                    <li><a href="#">Контакты</a></li>
+                    <li><a href="#">Заказ автовышки</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<!--Вставляем слайдер-->
+<?php
+    include "slider.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="m_header"><h1><p align="center"> Наши автовышки</p></h1></div>
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">
+            <p align="center">Автовышка 15 м</p>
+            <img class ="tbl_img" src="images/av-1.png" alt="" width="275" height="130">
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">2</div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">3</div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">4</div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">5</div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">6</div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">7</div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">8</div>
+    </div>
+</div>
+
+
+</body>
+</html>
---
+++ service.php
@@ -0,0 +1,42 @@
+<div id="carousel" class="carousel slide">
+    <!--Индикаторы слайдов-->
+    <ol class="carousel-indicators color-indic-0370">
+        <li class="active" data-target="#carousel" data-slide-to="0"></li>
+        <li data-target="#carousel" data-slide-to="1"></li>
+        <li data-target="#carousel" data-slide-to="2"></li>
+    </ol>
+
+
+    <!--Слайды-->
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="images/social_slider.png" alt="">
+            <div class="carousel-caption">
+                <!--<h3>Первый слайд</h3>
+                <p>Описание первого слайда</p>-->
+            </div>
+        </div>
+        <div class="item">
+            <img src="images/spasibo_slider.png" alt="">
+            <div class="carousel-caption">
+                <!--<h3>Второй слайд</h3>
+                <p>Описание второго слайда</p>-->
+            </div>
+        </div>
+        <div class="item">
+            <img src="images/vneocheredi_slider.png" alt="">
+            <div class="carousel-caption">
+                <!--<h3>Третий слайд</h3>
+                <p>Описание третьего слайда</p>-->
+            </div>
+        </div>
+    </div>
+
+    <!--Стрекли переключения слайдов-->
+    <a href="#carousel" class="left carousel-control" data-slide="prev">
+        <span class ="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a href="#carousel" class="right carousel-control" data-slide="next">
+        <span class ="glyphicon glyphicon-chevron-right"></span>
+    </a>
+</div>
