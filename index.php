5a6b518739d2065456e108a16714eb78e4daa9d9
Vladimir Makhmutov <makhmutov.va@mail.ru>
2016-08-19 12:08:57 +0700

    19.08.16

--- index.php
+++ index.php
@@ -63,10 +63,15 @@
 </div>
 -->
 
-
-
+<!--Вставляем слайдер-->
+<?php
+include "slider.php";
+?>
 <div class="navbar navbar-static-top">
+
     <div class="container">
+
+
         <div class="navbar-header">
 
             <button class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
@@ -89,10 +94,7 @@
     </div>
 </div>
 
-<!--Вставляем слайдер-->
-<?php
-    include "slider.php";
-?>
+
 
 <div class="container">
     <div class="row">
@@ -101,15 +103,36 @@
         </div>
         <div class="col-lg-3 col-md-4 col-sm-6 ">
             <p align="center">Автовышка 15 м</p>
-            <img class ="tbl_img" src="images/av-1.png" alt="" width="275" height="130">
+            <img class ="tbl_img" src="images/av-1.png" alt="" width="auto" height="auto">
         </div>
-        <div class="col-lg-3 col-md-4 col-sm-6 ">2</div>
-        <div class="col-lg-3 col-md-4 col-sm-6 ">3</div>
-        <div class="col-lg-3 col-md-4 col-sm-6 ">4</div>
-        <div class="col-lg-3 col-md-4 col-sm-6 ">5</div>
-        <div class="col-lg-3 col-md-4 col-sm-6 ">6</div>
-        <div class="col-lg-3 col-md-4 col-sm-6 ">7</div>
-        <div class="col-lg-3 col-md-4 col-sm-6 ">8</div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">
+            <p align="center">ISUZU ELF 15м</p>
+            <img class ="tbl_img" src="images/av-2.png" alt="" width="auto" height="auto">
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">
+            <p align="center">Mitsubishi Fuso 19м</p>
+            <img class ="tbl_img" src="images/av-3.png" alt="" width="auto" height="auto">
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">
+            <p align="center">ISUZU Forvard 19м</p>
+            <img class ="tbl_img" src="images/av-4.png" alt="" width="auto" height="auto">
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">
+            <p align="center">КамАЗ-4326 22м</p>
+            <img class ="tbl_img" src="images/av-5.png" alt="" width="auto" height="auto">
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">
+            <p align="center">ISUZU Forvard 25м</p>
+            <img class ="tbl_img" src="images/av-6.png" alt="" width="auto" height="auto">
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">
+            <p align="center">Hyundai HD 30м</p>
+            <img class ="tbl_img" src="images/av-7.png" alt="" width="auto" height="auto">
+        </div>
+        <div class="col-lg-3 col-md-4 col-sm-6 ">
+            <p align="center">КамАЗ-53215 34м</p>
+            <img class ="tbl_img" src="images/av-8.png" alt="" width="auto" height="auto">
+        </div>
     </div>
 </div>
 
